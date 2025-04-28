a4ff5f7b2243649aa8993f9a435c793162d70ae9
fahim-nion <fahimalhassan4@gmail.com>
2025-04-28 08:00:46 +0600

    classical weather app

---
+++ weather app/index.js
@@ -0,0 +1,63 @@
+const apiKey = "e3658f1af0be6b03bf176590fecb63ad";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+async function checkWeather(city) {
+    try {
+        const response = await fetch(apiUrl+ city +`&appid=${apiKey}`)
+        var data = await response.json()
+        // console.log(data)
+        console.log(data.weather[0].main);
+        
+        if (response.ok && data.cod == 200){
+            document.querySelector(".city").innerHTML = data.name
+            document.querySelector(".temp").innerHTML = `${data.main.temp}°C`
+            document.querySelector(".humidity").innerHTML = `${Math.floor(data.main.humidity)}%`
+            document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`
+        } else{
+            document.querySelector(".city").innerHTML = `Sorry, couldn't find the city`;
+            document.querySelector(".temp").innerHTML = ``;
+            document.querySelector(".humidity").innerHTML = ``;
+            document.querySelector(".wind").innerHTML = ``;
+        }
+        
+    } catch (error) {
+        console.log(error)
+        document.querySelector(".city").innerHTML = `Sorry, couldn't find the city`;
+        document.querySelector(".temp").innerHTML = ``;
+        document.querySelector(".humidity").innerHTML = ``;
+        document.querySelector(".wind").innerHTML = ``;
+    }
+
+    if ((data.weather[0].main) == "Clouds"){
+        document.querySelector(".weather-icon").src = "./imgw/cloudy-forecast-sun-svgrepo-com.svg"
+    }else if ((data.weather[0].main) == "Clear"){
+        document.querySelector(".weather-icon").src = "./imgw/sunny-sun-svgrepo-com.svg"
+    }else if ((data.weather[0].main) == "Rain"){
+        document.querySelector(".weather-icon").src = "./imgw/cloud-cry-emoticon-svgrepo-com.svg"
+    }else if ((data.weather[0].main) == "Drizzle"){
+        document.querySelector(".weather-icon").src = "./imgw/angry-cloud-emoticon-svgrepo-com.svg"
+    }else if ((data.weather[0].main) == "Haze"){
+        document.querySelector(".weather-icon").src = "./imgw/mist-svgrepo-com.svg"
+    }else if ((data.weather[0].main) == "Moderate Rain"){
+        document.querySelector(".weather-icon").src = "./imgw/fog-svgrepo-com.svg"
+    }
+    
+    document.querySelector(".details").style.display = "flex"
+
+
+}
+
+let srchBox = document.querySelector(".search").children[0]
+let srchBtn = document.querySelector(".search").children[1]
+
+srchBtn.addEventListener("click",()=>{
+    checkWeather(srchBox.value)
+})
+
+srchBox.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        checkWeather(srchBox.value);
+    }
+});
+
+//css e display none korar pore ekhane eshe display block kore dilei khuje pabo
