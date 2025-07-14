9d242358a6c86ffd4f0efeef0ea757aad3f08afc
fahim-nion <fahimalhassan4@gmail.com>
2025-07-14 10:56:17 +0600

    carousel project

--- image_carousel/index.js
+++ image_carousel/index.js
@@ -15,23 +15,44 @@ backBtn.addEventListener("click",()=>{
     scrollContainer.scrollLeft -= scrollContainer.clientWidth;
 })
 
-let random = Math.floor(0+(9-1)*Math.random())
-let random2 = Math.floor(0+(8-1)*Math.random())
-console.log(random)
+// let random = 
+// let random2 = 
+// console.log(random)
 
-let lttr = document.querySelector(".txt1").children[random]
-let lttr2 = document.querySelector(".txt2").children[random2]
+// let lttr = document.querySelector(".txt1").children[random]
+// let lttr2 = document.querySelector(".txt2").children[random2]
 
-setTimeout(() => {
-    changelumina()
-}, 300);
+// setTimeout(() => {
+//     changelumina()
+// }, 300);
 
-function changelumina(){
-    lttr.classList.add("lumina")
-    lttr2.classList.add("lumina")
+// function changelumina(){
+//     lttr.classList.add("lumina")
+//     lttr2.classList.add("lumina")
+//     setTimeout(() => {
+//     lttr.classList.remove("lumina")
+//     lttr2.classList.remove("lumina")
+//     }, 100);
+// }
+
+function changelumina() {
+    const txt1 = document.querySelector(".txt1").children;
+    const txt2 = document.querySelector(".txt2").children;
+
+    let random1 = Math.floor(0+(9-1)*Math.random());
+    let random2 = Math.floor(0+(8-1)*Math.random());
+
+    let lttr1 = txt1[random1];
+    let lttr2 = txt2[random2];
+
+    lttr1.classList.add("lumina");
+    lttr2.classList.add("lumina");
+
     setTimeout(() => {
-    lttr.classList.remove("lumina")
-    lttr2.classList.remove("lumina")
-    }, 100);
+        lttr1.classList.remove("lumina");
+        lttr2.classList.remove("lumina");
+    }, 150);
 }
 
+
+setInterval(changelumina, 180);
