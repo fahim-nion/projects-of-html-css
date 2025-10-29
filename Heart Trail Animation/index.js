e4d3a016056270424b57c33f31a2324a9bab4157
fahim-nion <fahimalhassan4@gmail.com>
2025-10-29 22:06:25 +0600

    back on doing projects on mern

--- Heart Trail Animation/index.js
+++ Heart Trail Animation/index.js
@@ -1,15 +1,34 @@
 const bodyEl = document.querySelector("body");
-bodyEl.addEventListener("mousemove",()=>{
-    const xPos = event.offsetX;
-    const yPos = event.offsetY;
+
+function createHeart(xPos, yPos) {
     const spanElem = document.createElement("span");
-    spanElem.style.left = xPos + "px"
-    spanElem.style.top = yPos + "px"
+    spanElem.style.left = xPos + "px";
+    spanElem.style.top = yPos + "px";
+    const size = Math.random() * 100;
+    spanElem.style.width = size + "px";
+    spanElem.style.height = size + "px";
     bodyEl.appendChild(spanElem);
-    const size = Math.random()*100;
-    spanElem.style.width = size +"px";
-    spanElem.style.height = size +"px";
     setTimeout(() => {
-        spanElem.remove()
+        spanElem.remove();
     }, 4000);
-})
+}
+
+
+bodyEl.addEventListener("mousemove", (event) => {
+    createHeart(event.offsetX, event.offsetY);
+});
+
+
+bodyEl.addEventListener("touchmove", (event) => {
+    const touch = event.touches[0];
+    const xPos = touch.clientX;
+    const yPos = touch.clientY;
+    createHeart(xPos, yPos);
+});
+
+bodyEl.addEventListener("touchstart", (event) => {
+    const touch = event.touches[0];
+    const xPos = touch.clientX;
+    const yPos = touch.clientY;
+    createHeart(xPos, yPos);
+});
