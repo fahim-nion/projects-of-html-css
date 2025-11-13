0e7edef62089b92a2320b1ce8ae9268a831b331f
fahim-nion <fahimalhassan4@gmail.com>
2025-11-13 06:25:59 +0600

    email subscription project

--- email-subscription/index.js
+++ email-subscription/index.js
@@ -1,12 +1,18 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzaWDV7yVEXOSl3UtMUzZZ3AZLY7PNMhcKKQokwmHtXN7_Hzbe_bDnAHyR76i41aVJSow/exec';
 const form = document.forms['submit-to-google-sheet'];
 const msg = document.getElementById("msg")
+msg.style.animation = "none"; // reset animation
+msg.offsetHeight; // trigger reflow
+msg.style.animation = "appear 5s linear"; 
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
     .then((response) => {
         msg.innerHTML = "Thank you for being the part of this great experience!"
+        msg.style.animation = "none";
+        msg.offsetHeight;
+        msg.style.animation = "appear 6s linear"; 
         setTimeout(() => {
             msg.innerHTML = ""
         }, 5000);
